afa97a408e7375b040f49274cc056d6674c87bbc
Maikel Suwisto <maikelsuwisto@gmail.com>
2025-09-10 15:44:43 +0700

    remove conn string

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
-import os
+from dotenv import load_dotenv
 
-# Railway provides DATABASE_URL directly
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:wnjQUAGFTDbmvCDHEUkJtUGfuCYbTTxU@shinkansen.proxy.rlwy.net:18315/railway?sslmode=require")
+# Load .env file
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 JWT_SECRET = os.getenv("JWT_SECRET", "supersecret")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
